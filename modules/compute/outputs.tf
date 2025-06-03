cf2594ead6f203f832f6ffb25f865ab05ed864c9
Ahmed <ahmedsikilli@gmail.com>
2025-06-03 14:28:39 +0100

    fix(edge): point CloudFront origin at EC2 public IP

--- modules/compute/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "frontend_private_ip" {
-  description = "Private IP of the compute instance"
-  value       = aws_instance.web.private_ip
-}
-
-output "frontend_public_ip" {
-  description = "Public IPv4 of the compute instance"
-  value       = aws_instance.web.public_ip
-}
